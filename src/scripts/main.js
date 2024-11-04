334632541b00232c344048695fe1396d45220109
Hibernon <hibernon73@gmail.com>
2024-11-04 19:41:59 -0300

    Mudança de data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("JUN 12, 2024 19:00:00");
+const dataDoEvento = new Date("JUN 12, 2025 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval (function(){
